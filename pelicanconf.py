f79ae87e3536782eb3bd9fd66bbb961d5d0bc01f
Jason K. Moore <moorepants@gmail.com>
2015-12-29 13:54:53 -0800

    Removed the social media crap.

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,16 +26,6 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
