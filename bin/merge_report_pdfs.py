eed54000a674b316bde1fe309350c735fa3c2d19
Jason K. Moore <moorepants@gmail.com>
2016-03-27 17:46:36 -0700

    Added directory argument.

--- bin/merge_report_pdfs.py
+++ bin/merge_report_pdfs.py
@@ -5,22 +5,23 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('d')
+parser.add_argument('directory')
 args = parser.parse_args()
 
-files = [f for f in os.listdir(args.d) if f.endswith('.pdf')]
+files = [f for f in os.listdir(args.directory) if f.endswith('.pdf')]
 graded = [f for f in files if f.endswith('graded.pdf')]
 
-pathy = lambda x: os.path.join(args.d, x)
+pathy = lambda x: os.path.join(args.directory, x)
 
 for f in graded:
     os.remove(pathy(f))
 
-files = [f for f in os.listdir(args.d) if f.endswith('.pdf')]
+files = [f for f in os.listdir(args.directory) if f.endswith('.pdf')]
 rubrics = sorted([f for f in files if f.endswith('rubric.pdf')])
 reports = sorted(list(set(files).difference(rubrics)))
 
 for report, rubric in zip(reports, rubrics):
     name = os.path.splitext(report)[0] + '-graded.pdf'
+    print(name)
     os.system("pdftk {} {} cat output {}".format(pathy(rubric), pathy(report),
                                                  pathy(name)))
