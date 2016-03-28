b3f3deefafbe1d55e4daa7f02005bd027c05c6ac
Jason K. Moore <moorepants@gmail.com>
2016-03-27 22:58:41 -0700

    Simplified the merge python script.

--- bin/merge_report_pdfs.py
+++ bin/merge_report_pdfs.py
@@ -1,27 +1,39 @@
 """Constructs a single PDF from the report and the rubric that was generated
-from Google Docs spreadsheet."""
+from Google Docs spreadsheet.
+
+python merge_report_pdfs <directory>
+
+"""
 
 import os
 import argparse
+from collections import defaultdict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('directory')
 args = parser.parse_args()
 
-files = [f for f in os.listdir(args.directory) if f.endswith('.pdf')]
-graded = [f for f in files if f.endswith('graded.pdf')]
+pdf_file_names = [f for f in os.listdir(args.directory) if f.endswith('.pdf')]
+
+file_data = defaultdict(lambda: {'report': False, 'rubric': False,
+                                 'graded': False})
+
+for f in pdf_file_names:
+    team_name, typ = os.path.splitext(f)[0].split('-')
+    if typ in ['report', 'rubric', 'graded']:
+        file_data[team_name][typ] = True
 
 pathy = lambda x: os.path.join(args.directory, x)
 
-for f in graded:
-    os.remove(pathy(f))
+# Remove any of the merged pdfs before creating a new one.
+for team_name, typ_dict in file_data.items():
+    graded_name = team_name + '-graded.pdf'
+    if typ_dict['graded']:
+        os.remove(pathy(graded_name))
+        print('{} deleted.'.format(graded_name))
 
-files = [f for f in os.listdir(args.directory) if f.endswith('.pdf')]
-rubrics = sorted([f for f in files if f.endswith('rubric.pdf')])
-reports = sorted(list(set(files).difference(rubrics)))
-
-for report, rubric in zip(reports, rubrics):
-    name = os.path.splitext(report)[0] + '-graded.pdf'
-    print(name)
-    os.system("pdftk {} {} cat output {}".format(pathy(rubric), pathy(report),
-                                                 pathy(name)))
+    if typ_dict['report'] and typ_dict['rubric']:
+        os.system("pdftk {} {} cat output {}".format(
+            pathy(team_name + '-rubric.pdf'), pathy(team_name + '-report.pdf'),
+            pathy(graded_name)))
+        print('{} created.'.format(graded_name))
