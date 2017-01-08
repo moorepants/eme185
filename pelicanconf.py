c0879dd468c4ba81951474694b901c35f5fe1b92
Jason K. Moore <moorepants@gmail.com>
2017-01-07 18:02:46 -0800

    Added headerid and build instructions.

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from os.path import join, expanduser
 
 AUTHOR = u'Jason K. Moore'
 SITENAME = u'EME 185: Mechanical Systems Design Project'
@@ -16,8 +17,11 @@ DEFAULT_LANG = u'en'
 
 TEMPLATE_PAGES = {'projects.html': 'projects.html'}
 
-PLUGIN_PATHS = ['plugins', '/moorepants/src/pelican-plugins']
-PLUGINS = ['neighbors']
+PLUGIN_PATHS = ["plugins", join(expanduser("~"), 'src', 'pelican-plugins')]
+PLUGINS = ['neighbors', 'headerid']
+
+# headerid options
+HEADERID_LINK_CHAR = "¶"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
