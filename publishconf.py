17e04b846f00ff879967823b005f52434c8506db
Kenneth Lyons <ixjlyons@gmail.com>
2017-01-10 13:48:34 -0800

    Use https url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://moorepants.github.io/eme185'
+SITEURL = 'https://moorepants.github.io/eme185'
 RELATIVE_URLS = False
 
 FEED_ATOM = None
