5c627ff509fdee5a7238e5d9fb0d4642315c4903
Jason K. Moore <moorepants@gmail.com>
2018-01-08 12:33:11 -0800

    Added autofilling dates to memos.

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 from os.path import join, expanduser
+from datetime import datetime
 
 AUTHOR = u'Jason K. Moore'
 SITENAME = u'EME 185: Mechanical Systems Design Project'
@@ -46,3 +47,17 @@ IGNORE_FILES = ['README*']
 POSTER_DUE_DATE = 'May 25, 2017 12 PM'
 SHOWCASE_REG_DATE = 'April 28, 2017'
 SHOWCASE_REG_URL = 'https://goo.gl/forms/IVpYlkIAlfUYblw32'
+DUE_DATES = \
+    {
+     'memo_01': '2018-01-12 22:00:00',
+     'memo_02': '2018-01-26 22:00:00',
+     'memo_03': '2018-02-02 22:00:00',
+     'memo_04': '2018-02-09 22:00:00',
+     'memo_05': '2018-03-02 22:00:00',
+     'report_01': '2018-02-16 22:00:00',
+     'report_02': '2018-03-19 22:00:00',
+     'report_03': '2018-05-04 22:00:00',
+     'report_04': '2018-06-10 22:00:00',
+    }
+DUE_DATES = {k: datetime.strptime(v, '%Y-%m-%d %H:%M:%S')
+             for k, v in DUE_DATES.items()}
