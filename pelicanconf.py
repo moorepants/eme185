cff071fdd18b756f677403a087ef994876acd9fa
Jason K. Moore <moorepants@gmail.com>
2018-12-27 17:48:43 -0800

    Updated due dates in conf file.

--- pelicanconf.py
+++ pelicanconf.py
@@ -55,24 +55,24 @@ MENUITEMS =[('Syllabus', ''),
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-SHOWCASE_REG_URL = 'https://ucdavis.co1.qualtrics.com/jfe/form/SV_1NvJvbejIFfjleR'
+SHOWCASE_REG_URL = 'https://'
 DUE_DATES = \
     {
-     'mem_01': '2018-01-12 22:00:00',
-     'mem_02': '2018-01-26 22:00:00',
-     'mem_03': '2018-02-02 22:00:00',
-     'mem_04': '2018-03-02 22:00:00',
-     'rep_01': '2018-02-16 22:00:00',
-     'rep_02': '2018-03-19 22:00:00',
-     'rep_03': '2018-05-04 22:00:00',
-     'rep_04': '2018-06-10 22:00:00',
-     'pre_01': '2018-03-05 00:00:00',  # design review 1
-     'pre_02': '2018-03-15 00:00:00',  # lightning talk
-     'pre_03': '2018-05-25 12:00:00',  # showcase poster
-     'pre_04': '2018-06-11 22:00:00',  # design review 2
-     'show_reg': '2018-04-27 23:59:59', # showcase registration
-     'show': '2018-06-07 13:30:00',  # showcase
-     'workroom': '2018-06-15 23:59:59',  # clear workroom
+     'mem_01': '2019-01-11 23:59:59',
+     'mem_02': '2019-01-25 23:59:59',
+     'mem_03': '2019-02-01 23:59:59',
+     'mem_04': '2019-03-01 23:59:59',
+     'rep_01': '2019-02-15 23:59:59',
+     'rep_02': '2019-03-18 23:59:59',
+     'rep_03': '2019-05-03 23:59:59',
+     'rep_04': '2019-06-09 23:59:59',
+     'pre_01': '2019-03-04 00:00:00',  # design review 1
+     'pre_02': '2019-03-14 00:00:00',  # lightning talk
+     'pre_03': '2019-05-24 12:00:00',  # showcase poster
+     'pre_04': '2019-06-10 23:59:59',  # design review 2
+     'show_reg': '2019-04-19 23:59:59',  # showcase registration
+     'show': '2019-06-06 13:00:00',  # showcase
+     'workroom': '2019-06-14 23:59:59',  # clear workroom
     }
 DUE_DATES = {k: datetime.strptime(v, '%Y-%m-%d %H:%M:%S')
              for k, v in DUE_DATES.items()}
